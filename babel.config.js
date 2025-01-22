1449d7241ab0ead8ce5475fe77b2926578ea2ce1
karan-jhinga <karanjhinga@gmail.com>
2025-01-23 01:43:11 +0530

    add tamagui compiler for optimisations

--- babel.config.js
+++ babel.config.js
@@ -4,6 +4,16 @@ module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
     [
+      '@tamagui/babel-plugin',
+      {
+        components: ['tamagui'],
+        config: './tamagui.config.ts',
+        importsWhitelist: ['constants.js', 'colors.js'],
+        logTimings: true,
+        disableExtraction: process.env.NODE_ENV === 'development',
+      }
+    ],
+    [
       'module-resolver',
       {
         root: ['./'],
