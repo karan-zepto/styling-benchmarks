3e0ae39b4da61efaa046fdc33a52c8f86d33bac3
karan-jhinga <karanjhinga@gmail.com>
2025-01-23 01:12:49 +0530

    add preset to enable profiling in production

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,22 @@
+const path = require('path');
+
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        resolvePath(sourcePath) {
+          if (sourcePath === '../implementations/ReactNativeRenderer-prod') {
+            return path.resolve(
+              __dirname,
+              './node_modules/react-native/Libraries/Renderer/implementations/ReactNativeRenderer-profiling',
+            );
+          }
+          return undefined;
+        },
+      },
+    ],
+  ],
 };
